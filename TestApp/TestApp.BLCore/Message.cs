4920c141fa47e3609b1f13e782070d678f6265bd
lgbkk <lgbkk@QUANTUM>
2016-05-27 22:51:50 +1000

    test

---
+++ TestApp/TestApp.BLCore/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.BLCore
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string MessageBody { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
